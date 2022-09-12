daca5a4a5cf57a07fb6bc2851d9ad3986d606a09
lehojae <lehojae@gmail.com>
2022-09-12 22:51:47 +0900

    #4 - 스프링부트 프로젝트 시작 스프링 프로젝트 초기화
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ my-project-board/src/test/java/com/myboard/myprojectboard/MyProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myboard.myprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
